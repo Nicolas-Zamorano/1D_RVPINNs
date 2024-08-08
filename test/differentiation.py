0c8f74acccbe63181ee54318b7500ace839be97e
Nikin <81661367+Nikinxd@users.noreply.github.com>
2024-08-08 12:18:05 -0400

    Create differentiation test
    test to validate that jacrev actually compute jacobian w.r.t inputs.

---
+++ test/differentiation.py
@@ -0,0 +1,44 @@
+import sys
+import torch
+
+sys.path.insert(0, "../src/")
+torch.set_default_device("cuda" if torch.cuda.is_available() else "cpu")
+torch.set_default_dtype(torch.float64)
+torch.cuda.empty_cache()
+
+from Neural_Network import Neural_Network
+
+def jacobian(NN_eval: torch.nn.Module,
+             x: torch.Tensor,
+             h: float = 0.03125):
+    """
+    Computes jacobian by finite difference method
+    
+    parameters:
+    - NN_eval (torch.nn.Module): NN model to evalute.
+    - x (torch.Tensor): Input.
+    - h (torch.Tensor): step size for finite difference method (default is  0.03125)
+    
+    Returns:
+    - jacobian(torch.Tensor): jacobian of NN_eval w.r.t. input.
+    """
+    
+    jacobian = (1/h) * (NN_eval(x+h) - NN_eval(x))    
+
+    return jacobian
+
+NN_ODE = Neural_Network(input_dimension = 1, 
+                    output_dimension = 2)
+
+ODE_example_point = torch.tensor([0.0]).unsqueeze(1)
+
+NN_ODE_jac = NN_ODE.jacobian(ODE_example_point)
+NN_ODE_jac_app = jacobian(NN_ODE.evaluate, 
+                          ODE_example_point)
+error_ODE = torch.sum(abs(NN_ODE_jac - NN_ODE_jac_app))
+
+
+print("Error of differenciation of ODE Neural Network:", error_ODE.item())
+
+
+
