ca049a5593d6b9e06f054aae41e20a71ee531624
Nikin <81661367+Nikinxd@users.noreply.github.com>
2024-08-09 16:21:00 -0400

    Update  RK4
    update RK4 to have parameters

--- utils/RK4.py
+++ utils/RK4.py
@@ -1,6 +1,6 @@
 import torch
 
-def RK4(function, initial_value, collocation_points):
+def RK4(function, initial_value, collocation_points, parameters):
     """
     Solve initial value Problem (IVP) using 4th order Runge-Kutta.
     
@@ -8,34 +8,37 @@ def RK4(function, initial_value, collocation_points):
     -function: function of IVP. it has to have form: f(t,x).
     -initial_value: initial value of IVP.
     -collocation_points: values it witch aproximate the solution of IVP.
+    -parameters: parameters of IVP.
     
     return:
     -solution_values: values of the solution in collocation_points.
     """
     
-    solution_values = [initial_value]
+    solution_values = torch.zeros([collocation_points.size(0), initial_value.size(1)])
+    
+    solution_values[0] = initial_value
     
     N = len(collocation_points)
     
+    h = collocation_points[1:] - collocation_points[:-1]
+    
     for i in range(1, N):
-        print(f"Computing Exact Solution: {i} of {N}", end='',flush = True)
+        print(f"\rComputing Exact Solution: {i} of {N}", end='',flush = True)
         
         t_prev = collocation_points[i-1]
         t_next = collocation_points[i]
-        h = t_next - t_prev
         
-        x_prev = solution_values[-1]
+        x_prev = solution_values[-1].unsqueeze_(0)
         
-        k1 = function(t_prev, x_prev)
-        k2 = function(t_prev + h/2, x_prev + h*k1/2)
-        k3 = function(t_prev + h/2, x_prev + h*k2/2)
-        k4 = function(t_next, x_prev + h*k3)
+        k1 = function(t_prev, x_prev, parameters)
+        k2 = function(t_prev + h[i-1]/2, x_prev + h[i-1]*k1/2, parameters)
+        k3 = function(t_prev + h[i-1]/2, x_prev + h[i-1]*k2/2, parameters)
+        k4 = function(t_next, x_prev + h[i-1]*k3, parameters)
         
-        x_next = x_prev + h/6 * (k1 + 2*k2 + 2*k3 + k4)
-        solution_values.append(x_next)
+        solution_values[i] = x_prev + h[i-1]/6 * (k1 + 2*k2 + 2*k3 + k4)
         
     print("")
     del function, initial_value, collocation_points
-    del N, t_prev, t_next, h, x_prev, k1, k2, k3, k4, x_next 
+    del N, t_prev, t_next, h, x_prev, k1, k2, k3, k4 
     
-    return torch.stack(solution_values)
+    return solution_values
