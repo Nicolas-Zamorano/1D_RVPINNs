611c716a0ab53ccc957c4cf621d32d06c919de1d
Nikin <81661367+Nikinxd@users.noreply.github.com>
2024-09-05 19:35:48 -0400

    move src and utils
    move src and utils in preparation to create a library

---
+++ RVPINNs/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RVPINNs',
+    version='0.1',
+    description='a python libary to solve Diferential Equations with VPINNs.',
+    author='Nicolás Zamorano',
+    packages=find_packages(),
+)
